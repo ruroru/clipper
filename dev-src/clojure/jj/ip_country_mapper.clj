dfb177f82f54272425da8ba7083ce4fc63229fd5
jj <keistaslogairkosulys@gmail.com>
2025-12-05 20:13:14 +0200

    Rewrite to make it smoother
    # Conflicts:
    #	project.clj

--- dev-src/clojure/jj/ip_country_mapper.clj
+++ dev-src/clojure/jj/ip_country_mapper.clj
@@ -1,19 +1,20 @@
 (ns jj.ip-country-mapper
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
+            [clojure.string :as str]
             [clojure.tools.logging :as logger]
+            [clojure.pprint :as pprint]
             [jj.iso.countries :as countries]))
 
 (defn parse-csv-data [file-resource]
   (with-open [reader (io/reader file-resource)]
     (let [csv-data (csv/read-csv reader)]
-      (into {}
-            (doall
-              (map (fn [row]
-                     (let [network-str (first row)
-                           alpha-2-name (nth row 4)]
-                       [network-str (countries/alpha-2->name alpha-2-name)]))
-                   (rest csv-data)))))))
+      (doall
+        (map (fn [row]
+               (let [network-str (first row)
+                     alpha-2-name (nth row 4)]
+                 [network-str (countries/alpha-2->name alpha-2-name)]))
+             (rest csv-data))))))
 
 (defn download-file
   [file-url download-location]
@@ -24,10 +25,73 @@
     (catch Exception e
       (logger/error (str "Error downloading file: " (.getMessage e))))))
 
-(defn create-map-edn-file []
+(defn get-first-octet [network-str]
+  (-> network-str
+      (clojure.string/split #"\.")
+      first))
+
+(defn group-by-first-octet [parsed-data]
+  (reduce (fn [acc [network-str country]]
+            (let [octet (get-first-octet network-str)]
+              (update acc octet (fnil conj []) [network-str country])))
+          {}
+          parsed-data))
+
+(defn ip->long [ip]
+  (let [parts (mapv #(Long/parseLong %) (str/split ip #"\."))]
+    (+ (bit-shift-left (parts 0) 24)
+       (bit-shift-left (parts 1) 16)
+       (bit-shift-left (parts 2) 8)
+       (parts 3))))
+
+(defn cidr->range [cidr]
+  (let [[ip prefix] (str/split cidr #"/")
+        prefix-len (Long/parseLong prefix)
+        start (ip->long ip)
+        mask (bit-and (bit-shift-left 0xFFFFFFFF (- 32 prefix-len)) 0xFFFFFFFF)
+        network-start (bit-and start mask)
+        network-end (bit-or network-start (bit-and (bit-not mask) 0xFFFFFFFF))]
+    [network-start network-end]))
+
+(defn ip-allocation-nested [cidr-map]
+  (let [grouped (group-by val cidr-map)]
+    (reduce-kv (fn [[countries counts] country cidrs]
+                 (let [ip-counts (mapv (fn [[cidr _]]
+                                         (let [[start end] (cidr->range cidr)]
+                                           (inc (- end start))))
+                                       cidrs)
+                       count-val (if (= 1 (count ip-counts))
+                                   (first ip-counts)
+                                   ip-counts)]
+                   [(conj countries country)
+                    (conj counts count-val)]))
+               [[] []]
+               grouped)))
+
+(defn create-map-edn-files []
   (let [url "https://r2.datahub.io/clt98minh000ol708cfwakoxt/main/raw/data/geoip2-ipv4.csv"
-        csv-file-location "./target/file.csv"]
+        csv-file-location "./target/file.csv"
+        output-dir "./src/resources/ip-maps"]
     (download-file url csv-file-location)
-    (let [parsed-data (parse-csv-data "./target/file.csv")]
-      (spit "./src/resources/map.edn" (assoc parsed-data "127.0.0.1/32" :localhost)))))
-(create-map-edn-file)
+
+    (.mkdirs (io/file output-dir))
+
+    (let [parsed-data (parse-csv-data csv-file-location)
+          grouped-data (group-by-first-octet parsed-data)]
+
+      (doseq [[octet entries] grouped-data]
+        (let [file-path (str output-dir "/" octet ".edn")
+              data-map (into {} entries)]
+          (with-open [w (io/writer file-path)]
+            (binding [*out* w]
+              (pprint/pprint (ip-allocation-nested data-map) )))
+          (logger/info (str "Written " (count entries) " entries to " file-path))))
+
+      (let [localhost-file (str output-dir "/127.edn")]
+        (with-open [w (io/writer localhost-file)]
+          (binding [*out* w]
+            (pprint/pprint (merge (read-string (slurp localhost-file))
+                                  {"127.0.0.1/32" :localhost}))))))))
+
+(defn -main [& _]
+  (create-map-edn-files))
--- src/clojure/main/jj/clipper.clj
+++ src/clojure/main/jj/clipper.clj
@@ -1,20 +1,50 @@
 (ns jj.clipper
-  (:require [clojure.edn :as edn]
-            [clojure.tools.logging :as logger]
-            [clojure.java.io :as io])
-  (:import (java.io PushbackReader)
-           (jj IpLocationRegistry)))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.edn :as edn]
+            [clojure.tools.logging :as logger]))
 
+(defn- parse-octets [ip]
+  (when ip
+    (mapv #(Integer/parseInt %) (str/split ip #"\."))))
 
-(def ip-mapper (atom nil))
+(defn- get-first-octet [ip]
+  (when ip
+    (-> ip
+        (str/split #"\.")
+        first)))
+
+(defn- find-location [ranges octets]
+  (when (and (seq ranges) (seq octets))
+    (let [octet-value (first octets)
+          remaining-octets (rest octets)]
+      (loop [remaining ranges
+             accumulated 0]
+        (when (seq remaining)
+          (let [[range-size location] (take 2 remaining)
+                new-accumulated (+ accumulated range-size)]
+            (if (< octet-value new-accumulated)
+              (cond
+                (vector? location)
+                (find-location location remaining-octets)
+
+                (= location :not-assigned)
+                nil
+
+                :else
+                location)
+              (recur (drop 2 remaining) new-accumulated))))))))
 
 (defn locate [ip]
-  (when (nil? @ip-mapper)
-    (reset! ip-mapper
-            (IpLocationRegistry. (edn/read (PushbackReader. (io/reader (io/resource "map.edn")))))))
-
-  (when (instance? String ip)
-    (if @ip-mapper
-      (.locate ^IpLocationRegistry @ip-mapper ^String ip)
-      (logger/error "Unable to find CSV file"))))
-
+  (when (some? ip)
+    (when-let [octet (get-first-octet ip)]
+      (if-let [resource (io/resource (str "jj/clipper/" octet ".edn"))]
+        (let [content (-> resource
+                          slurp
+                          str/trim
+                          edn/read-string)]
+          (if (keyword? content)
+            content
+            (when-let [octets (parse-octets ip)]
+              (find-location content (rest octets)))))
+        (logger/error "Unable to find " (str "jj/clipper/" octet ".edn"))))))
